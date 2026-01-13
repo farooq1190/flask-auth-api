8b4552ccd04397e441321d1e0f0ebb4bf620acb7
farooq1190 <fkfarooqkhan1190@gmail.com>
2026-01-13 23:10:38 +0530

    change

--- models.py
+++ models.py
@@ -12,7 +12,8 @@ def get_user_by_username(username):
 
 def create_user(username, password_hash):
     conn = get_db_connection()
-    conn.execute(
+    cursor = conn.cursor()
+    cursor.execute(
         "INSERT INTO users (username, password) VALUES (?, ?)",
         (username, password_hash)
     )
